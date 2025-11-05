49ad1ef6a9d946aecd414fdae4db1e84ee1f4f32
Kevin Königseder <80044581+Koenigseder@users.noreply.github.com>
2025-11-05 14:01:35 +0100

    fix(git): check if state changed (#18)

--- internal/git/commit_push.go
+++ internal/git/commit_push.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // CommitAndPushFiles commits and pushes all untracked files in the BADM Git repository
 func CommitAndPushFiles(repoPath, commitMsg string) {
+	filesChanged, err := checkIfFilesChanged(repoPath)
+	if err != nil {
+		fmt.Println("Unable checking file status:", err)
+		os.Exit(1)
+	}
+
+	if !filesChanged {
+		fmt.Println("No files changed! Everything's up to date :)")
+		os.Exit(0)
+	}
+
 	// git add
-	_, err := exec.Command("git", "-C", repoPath, "add", ".").Output()
+	_, err = exec.Command("git", "-C", repoPath, "add", ".").Output()
 	if err != nil {
 		fmt.Println("Unable adding Dotfile to Git:", err)
 		os.Exit(1)
@@ -28,4 +40,17 @@ func CommitAndPushFiles(repoPath, commitMsg string) {
 		fmt.Println("Unable pushing Dotfile to remote Git repository:", err)
 		os.Exit(1)
 	}
+}
+
+func checkIfFilesChanged(repoPath string) (bool, error) {
+	output, err := exec.Command("git", "-C", repoPath, "status").Output()
+	if err != nil {
+		return false, err
+	}
+
+	if strings.Contains(string(output), "nothing to commit") {
+		return false, nil
+	}
+
+	return true, nil
 }
